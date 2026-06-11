bb27952d85613c0ceae538dbc2151b63884ef0e8
allanhue <allanmwangi329@gmail.com>
2026-06-11 17:50:01 +0300

    added databse connection

--- galleria_back/db.go
+++ galleria_back/db.go
@@ -1,1 +1,23 @@
-db=
+package db
+
+import (
+    "log"
+    "os"
+
+    "gorm.io/driver/postgres"
+    "gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func Connect() {
+    dsn := os.Getenv("DATABASE_URL")
+    
+    database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+    if err != nil {
+        log.Fatal("Failed to connect to database:", err)
+    }
+
+    DB = database
+    log.Println("Database connected")
+}
--- galleria_back/main.go
+++ galleria_back/main.go
@@ -1,10 +1,24 @@
 package main
 
 import (
+    "gatherly/backend/db"
+    "gatherly/backend/models"
     "github.com/gin-gonic/gin"
+    "github.com/joho/godotenv"
+    "log"
 )
 
 func main() {
+    err := godotenv.Load()
+    if err != nil {
+        log.Fatal("Error loading .env file")
+    }
+
+    db.Connect()
+
+    // Auto create tables
+    db.DB.AutoMigrate(&models.User{})
+
     r := gin.Default()
 
     r.GET("/health", func(c *gin.Context) {
